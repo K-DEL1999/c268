46d6ab7999f2456470294f13f62a7244b505e769
kdeleon000@citymail.cuny.edu <kdeleon000@citymail.cuny.edu>
2022-09-23 14:17:28 -0400

    Update

--- Activity8.sh
+++ Activity8.sh
@@ -6,24 +6,27 @@ Array=(0,0,0,0,0,0,0,0,0,0)
 
 echo "Please enter 5-10 numbers (input 'D' when done): "
 
-while [[ size -lt 4 ]] || [[ size -lt 10 ]] 
+while [[ size -lt 5 ]] || [[ size -lt 10 ]] 
 do
 	read number
 
-	if [[ $number == "D" ]]
+	if [[ $number == "D" ]] && [[ size -gt 4 ]]
 	then 
 		break
 	fi
 
-	if [[ $number =~ [0-9] ]]
+	if [[ $number =~ [0-9] ]]  
 	then
 		((size++))
 		Array[(($size-1))]=$number
+		echo "next value: "
 	else
 		echo "invalid value::please input again:"
 	fi
 done
 
-echo ${Array[@]} 
+#echo ${Array[@]} 
+
+echo $(product Array)
 
 exit 0
