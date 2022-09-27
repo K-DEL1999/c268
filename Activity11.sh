d1d5317514921298c3224dbde0e9992948098f6d
EC2 Default User <kdeleon000@citymail.cuny.edu>
2022-09-27 18:00:55 +0000

    Update

--- Activity11.sh
+++ Activity11.sh
@@ -1,22 +1,5 @@
 #! /bin/bash
 
-function ReverseSort(){
-	local -n Array=$1
-	
-	for i in ${!Array[@]} 
-	do
-		for ((j=$i+1;j<$2;j++))
-		do
-			if [[ ${Array[$j]} -gt ${Array[$i]} ]]
-			then
-				temp=${Array[$j]}
-				Array[$j]=${Array[$i]}
-				Array[$i]=$temp
-			fi
-		done
-	done
-}
-
 if [[ -f "Numbers.txt" ]]
 then
 	rm "Numbers.txt"
@@ -24,31 +7,13 @@ fi
 
 touch "Numbers.txt"
 
-echo 5 6 3 7 4 4 9 6 7 9 8 2 3 6 4 5 1 1 3 10 12 > Numbers.txt
-NonDupArray=()
-size=0
-readarray -t Nums < Numbers.txt
-echo "Before: ${Nums[@]}"
+Array=(5 6 3 7 4 4 9 6 7 9 8 2 3 6 4 5 1 1 3 10 12)
 
-#DupRemover
-#---------------------------------------------
-for i in ${Nums[@]}
+for i in ${Array[@]}
 do
-	#echo $i 
-	if [[ ! ${NonDupArray[@]} =~ $i ]]
-	then
-		NonDupArray[size]=$i
-		((size++))
-	fi
+	echo $i >> Numbers.txt
 done
-#---------------------------------------------
-echo "After: ${NonDupArray[@]}"
 
-#ReverseSorter
-#----------------------------------
-ReverseSort NonDupArray size
-#
-
-echo "Final: ${NonDupArray[@]}"
+cat Numbers.txt | sort -rn | uniq
 
 exit 0
